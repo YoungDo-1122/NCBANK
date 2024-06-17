acae1250874dd387913d435894e08a2a476dbde5
YoungDo-1122 <dudeh333221@naver.com>
2024-06-17 11:12:02 +0900

    24.06.17_01_Test

--- Bank/src/main/java/ncbank/service/BoardService.java
+++ Bank/src/main/java/ncbank/service/BoardService.java
@@ -1,6 +1,5 @@
 package ncbank.service;
 
-import java.io.File;
 import java.util.List;
 
 import javax.annotation.Resource;
@@ -10,7 +9,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.stereotype.Service;
-import org.springframework.web.multipart.MultipartFile;
 
 import ncbank.beans.ContentBean;
 import ncbank.beans.PageBean;
@@ -36,30 +34,7 @@ public class BoardService {
 	@Resource(name = "loginUserBean")
 	private UserBean loginUserBean;
 
-	private String saveUploadFile(MultipartFile upload_file) {
-
-		String file_name = System.currentTimeMillis() + "_"
-				+ FilenameUtils.getBaseName(upload_file.getOriginalFilename()) + "."
-				+ FilenameUtils.getExtension(upload_file.getOriginalFilename());
-
-		try {// 경로와 파일네임
-			upload_file.transferTo(new File(path_upload + "/" + file_name));
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-
-		return file_name;
-	}
-
 	public void addContentInfo(ContentBean writeContentBean) {
-
-		MultipartFile upload_file = writeContentBean.getUpload_file();
-
-		if (upload_file.getSize() > 0) {
-			String file_name = saveUploadFile(upload_file);
-			System.out.println(file_name);
-			writeContentBean.setContent_file(file_name); // 오라클에 파일 이름 저장
-		}
 
 		writeContentBean.setContent_writer_idx(loginUserBean.getUser_num());
 
@@ -80,34 +55,30 @@ public class BoardService {
 
 		return boardDao.getContentList(board_info_idx, rowBounds);
 	}
-	
+
 	public ContentBean getContentInfo(int content_idx) {
 		return boardDao.getContentInfo(content_idx);
 	}
-	
-	//페이지 관련
-	public PageBean getContentCnt(int content_board_idx,int currentPage) {
+
+	// 페이지 관련
+	public PageBean getContentCnt(int content_board_idx, int currentPage) {
 		int contentCnt = boardDao.getContentCnt(content_board_idx);
-		
+
 		PageBean pageBean = new PageBean(contentCnt, currentPage, page_listcnt, page_paginationcnt);
 		return pageBean;
 	}
 
-
-
 	/*
-	// 이미지 처리 때문에 서비스 들어와야 함
-	public void modifyContentInfo(ContentBean modifyContentBean) {
-
-		MultipartFile uploadFile = modifyContentBean.getUpload_file();
-
-		if (uploadFile.getSize() > 0) {
-			String fileName = saveUploadFile(uploadFile);
-			modifyContentBean.setContent_file(fileName);
-		}
-
-		
-	}
-	*/
+	 * // 이미지 처리 때문에 서비스 들어와야 함 public void modifyContentInfo(ContentBean
+	 * modifyContentBean) {
+	 * 
+	 * MultipartFile uploadFile = modifyContentBean.getUpload_file();
+	 * 
+	 * if (uploadFile.getSize() > 0) { String fileName = saveUploadFile(uploadFile);
+	 * modifyContentBean.setContent_file(fileName); }
+	 * 
+	 * 
+	 * }
+	 */
 
 }
