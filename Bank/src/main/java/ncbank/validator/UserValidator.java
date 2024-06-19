600577bfaae325a1018a777213e4a9ff216e66ef
YoungDo-1122 <dudeh333221@naver.com>
2024-06-19 17:35:47 +0900

    YoungDo_02(24.06.19)

--- Bank/src/main/java/ncbank/validator/UserValidator.java
+++ Bank/src/main/java/ncbank/validator/UserValidator.java
@@ -7,7 +7,7 @@ import ncbank.beans.UserBean;
 
 public class UserValidator implements Validator {
 
-	@Override	
+	@Override
 	public boolean supports(Class<?> clazz) {
 		return UserBean.class.isAssignableFrom(clazz);
 	}
@@ -17,11 +17,13 @@ public class UserValidator implements Validator {
 		UserBean loginBean = (UserBean) target;
 
 		String beanName = errors.getObjectName();
-		System.out.println(beanName); // joinUserBean
-
+		System.out.println(beanName + "1");
 		if (beanName.equals("mBean")) {
-
+			System.out.println(beanName + "2");
 			if (!loginBean.getPwd().equals(loginBean.getPwd2())) {
+				System.out.println(beanName + "3");
+				System.out.println(loginBean.getPwd() + "4");
+				System.out.println(loginBean.getPwd() + "4");
 				errors.rejectValue("pwd", "NotEquals");
 				errors.rejectValue("pwd2", "NotEquals");
 			}
